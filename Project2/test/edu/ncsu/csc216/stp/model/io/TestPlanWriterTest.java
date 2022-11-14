a99d0c32c42557a6b3779296d4d59b92f425a715
Jimin Yu <jyu34@ncsu.edu>
2022-11-13 22:37:01 -0500

    [Implementation] Working on TestPlanWriter

--- Project2/test/edu/ncsu/csc216/stp/model/io/TestPlanWriterTest.java
+++ Project2/test/edu/ncsu/csc216/stp/model/io/TestPlanWriterTest.java
@@ -13,7 +13,10 @@ import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
+import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
 import edu.ncsu.csc216.stp.model.tests.TestCase;
+import edu.ncsu.csc216.stp.model.util.ISortedList;
+import edu.ncsu.csc216.stp.model.util.SortedList;
 
 /**
  * @author stbeuav
@@ -27,10 +30,30 @@ class TestPlanWriterTest {
 	@Test
 	void testWriteTestPlanFile() {
 		//fail("Not yet implemented");
+		TestPlan plan1 = new TestPlan("TestPlan1");
+		TestPlan plan2 = new TestPlan("TestPlan2");
+		
 		TestCase t0 = new TestCase("ID 0", "type 0", "description 0", "expected results 0");
 		TestCase t1 = new TestCase("ID 1", "type 1", "description 1", "expected results 1");
 		TestCase t2 = new TestCase("ID 2", "type 2", "description 2", "expected results 2");
 		t0.addTestResult(false, "actual results 0-1");
+		t1.addTestResult(true, "PASS: actual results 1-1");
+		t1.addTestResult(true, "FAIL: actual results 1-2");
+		t2.addTestResult(true, "PASS: actual results 2-1");
+		t2.addTestResult(true, "PASS: actual results 2-2");
+		
+		plan1.addTestCase(t0);
+		plan1.addTestCase(t1);
+		plan1.addTestCase(t2);
+		
+		ISortedList<TestPlan> tp = new SortedList<TestPlan>();
+		tp.add(plan1);
+		tp.add(plan2);
+		
+		TestPlanWriter.writeTestPlanFile("test-files/actual_out.txt", tp);
+		checkFiles("test-files/expected_out.txt", "test-files/actual_out.txt");
+		
+		assertThrows(IllegalArgumentException.class, () -> TestPlanWriter.writeTestPlanFile("", tp));
 		
 	}
 	
