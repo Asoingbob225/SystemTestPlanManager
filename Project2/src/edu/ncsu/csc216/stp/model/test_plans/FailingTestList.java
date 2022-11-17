5f2210a0997730bbf779ed05d7bc2b5028297845
Levi Curlee <lacurlee@ncsu.edu>
2022-11-17 00:53:28 -0500

    [Debugging] Fixed error in FailingTestList

--- Project2/src/edu/ncsu/csc216/stp/model/test_plans/FailingTestList.java
+++ Project2/src/edu/ncsu/csc216/stp/model/test_plans/FailingTestList.java
@@ -67,7 +67,11 @@ public class FailingTestList extends AbstractTestPlan {
 		for (int i = 0; i < getTestCases().size(); i++) {
 			array[i][0] = getTestCases().get(i).getTestCaseId();
 			array[i][1] = getTestCases().get(i).getTestType();
-			array[i][2] = getTestCases().get(i).getTestPlan().getTestPlanName();
+			if (getTestCases().get(i).getTestPlan().getTestPlanName() == null) {
+				array[i][2] = "";
+			} else {
+				array[i][2] = getTestCases().get(i).getTestPlan().getTestPlanName();
+			}
 		}
 		return array;
 	}
